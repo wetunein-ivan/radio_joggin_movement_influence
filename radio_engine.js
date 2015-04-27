1660aac244cf2b35d16d2c795bf1bb221386509f
Jesus Sanz Marcos <jesus.sanzmarcos@activenetwork.com>
2015-04-27 02:49:52 +0200

    fix

---
+++ radio_engine.js
@@ -0,0 +1,12 @@
+{
+  "name":"Jazz 911",
+  "version":"1.2.3",
+  "categories":["Jazz","@Sports"],
+  "tags":["Jazz","Weather"],
+  "description" : "This is a first example of the radio engine V1. It randomly shows music based on current weather temperature.",
+  "developer" :
+    {
+      "name":"JSM",
+      "company":"RR Music"
+    },
+}
