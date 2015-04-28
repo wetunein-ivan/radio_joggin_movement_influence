df9aca54a293e574a5182953c1a2e5e04291b952
wetunein-ivan <ivan.s@wetunein.com>
2015-04-28 16:44:04 +0200

    Update radio_engine.js

--- radio_engine.js
+++ radio_engine.js
@@ -1,9 +1,9 @@
 {
-  "name":"Jazz POC",
+  "name":"Driving Electronic Mix",
   "version":"1.2.3",
   "categories":["Jazz","@Sports"],
   "tags":["Jazz","Weather"],
-  "description" : "It starts with random jazz tracks of RED mood but when you press the LIKE button it will generate a radio station for this track.",
+  "description" : "Random Electronics Tracks that vary the mood depending on car speed.",
   "developer" :
     {
       "name":"JSM",
